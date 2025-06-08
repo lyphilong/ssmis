bbf7e0f9ed1f2308e7b4c5c183ef2b28b54a4f5d
Hwi-Tran <trandanghuy13456@gmail.com>
2025-06-08 12:47:06 +0700

    remove topo weight

--- code/run_brats19.sh
+++ code/run_brats19.sh
@@ -3,15 +3,14 @@
 # Main training script
 python train_DyCON_BraTS19.py \
 --root_dir "../data/BraTS2019" \
---exp "BraTS2019-topo-0.3" \
+--exp "BraTS2019-topo-2d-only" \
 --model "unet_3D" \
---max_iterations 0 \
+--max_iterations 2000 \
 --temp 0.6 \
 --batch_size 8 \
 --labelnum 25 \
 --gpu_id 1 \
 --use_topo_loss 1 \
---topo_weight 0.3 \
 --topo_size 32 \
 --pd_threshold 0.3 \
 --topo_rampup 500 
